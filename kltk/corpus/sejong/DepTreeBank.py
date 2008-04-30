75c786d53204c6dcae34cedba4f93070cd07ad72
youhyunjo <youhyunjo@gmail.com>
2008-04-30 15:08:33 +0000

    kltk.corpus.sejong.DepTreeBank   - implemented Dep2FS and Dep2TrXML   - Dep2FS converts whole dependency treebank (77k sentences) successfully 	into TrEd .fs format.
    git-svn-id: https://kltk.googlecode.com/svn/trunk@23 dc14f8f6-e949-0410-ab7c-05e6b6b323a2

--- kltk/corpus/sejong/DepTreeBank.py
+++ kltk/corpus/sejong/DepTreeBank.py
@@ -13,12 +13,12 @@ class DepWalker:
 	def readtree(self):
 
 		line = self.readline()
-		tree = Tree(line)
+		tree = Tree(line.split(' ')[0])
 
 		line = self.readline()
 		while (line):
 			(ord, dep, tag1, tag2, form) = line.split("\t")	
-			tree.nodes.append(Node(ord, dep, tag1, tag2, form))
+			tree.nodes.append(Node(ord, dep, tag1, tag2, form.strip()))
 			line = self.readline()
 
 		return tree
@@ -65,46 +65,142 @@ class Encode:
 
 
 
-class Test:
+class Dep2FS:
+	def __init__(self, file, output_encoding):
+		self.dw = DepWalker(file)
+		sys.stdout = Encode(sys.stdout, output_encoding)
+		self.dep2fs()
+		
+	def dep2fs(self) :
+		self.print_header()
+		for tree in self.dw:
+			self.print_nd_open(tree.id, "", "", 0, 0)
+			sys.stdout.write("(")
+			root = self.find_root(tree.nodes) 
+			list = tree.nodes
+			list.remove(root)
+			self.print_node(list, root, 1)
+			print ")"
+		
+	def find_root(self, list):
+		for n in list:
+			if n.ord == n.dep:
+				return n
+
+	def print_node(self, list, node, depth):
+		self.print_nd_open(node.form, node.tag1, node.tag2, node.ord, depth)
+		children = []
+		for n in list:
+			if n.dep == node.ord:
+				children.append(n)
+
+		if len(children) > 0:
+			sys.stdout.write("(")
+			while(children) :
+				n = children.pop(0)
+				self.print_node(list, n, depth+1)
+				if(len(children) > 0) : sys.stdout.write(",")
+			sys.stdout.write(")")
+
+
+
+	def print_nd_open(self, form, tag1, tag2, ord, depth):
+		sys.stdout.write("[%s,%s,%s,%s]" % (form.replace(",","\\,").replace("]","\\]").replace("[","\\["), tag1.replace("]","\\]").replace("[","\\["), tag2.replace("]","\\]").replace("[","\\["), str(ord)))
+		
+	#	"[" + form.replace(",","\\,")  \
+# 				+ "," + tag1 \
+# 				+ "," + tag2 \
+# 				+ "," + str(ord)  \
+# 		     	+ "]"
+# 
+	def print_header(self):
+		print """@E utf-8
+@P form
+@P afun
+@P tag
+@N ord
+"""
+
+
+
+class Dep2TrXML:
 	def __init__(self, file, output_encoding):
 		self.dw = DepWalker(file)
 		sys.stdout = Encode(sys.stdout, output_encoding)
 		self.dep2trxml()
 		
 	def dep2trxml(self) :
+		self.print_header()
 		for tree in self.dw:
-			self.print_nd_open(tree.id, "", "", 0)
+			self.print_nd_open(tree.id, "", "", 0, 0)
 			root = self.find_root(tree.nodes) 
 			list = tree.nodes
 			list.remove(root)
-			self.print_node(list, root)
+			self.print_node(list, root, 1)
 			print "</nd>"
+		print "</trees>"
 
 
+		
 	def find_root(self, list):
 		for n in list:
 			if n.ord == n.dep:
 				return n
 
-	def print_node(self, list, node):
-		self.print_nd_open(node.form, node.tag1, node.tag2, node.ord)
-		#list.remove(node)
+	def print_node(self, list, node, depth):
+		self.print_nd_open(node.form, node.tag1, node.tag2, node.ord, depth)
+		has_child = False
 		for n in list:
 			if n.dep == node.ord:
-				self.print_node(list, n)
-				pass
-		print "</nd>"
+				self.print_node(list, n, depth+1)
+				has_child = True
+		print depth*"\t" + "</nd>"
 
 
 
-	def print_nd_open(self, form, tag1, tag2, ord):
-		print "<nd form=\"" + form  \
-				+ "\" tag1=\"" + tag1 \
-				+ "\" tag2=\"" + tag2 \
-				+ "\" ord=" + str(ord)  \
-		     	+ ">"
+	def print_nd_open(self, form, tag1, tag2, ord, depth):
+		print depth*"\t" + "<nd form='" + form  \
+				+ "' afun='" + tag1 \
+				+ "' tag='" + tag2 \
+				+ "' ord='" + str(ord)  \
+		     	+ "'>"
+
+	def print_header(self):
+		print """		
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE trees PUBLIC "-//CKL.MFF.UK//DTD TrXML V1.0//EN" "http://ufal.mff.cuni.cz/~pajas/tred.dtd" [
+<!ENTITY % trxml.attributes "  
+  token CDATA #IMPLIED
+  label CDATA #IMPLIED
+  tag_1 CDATA #IMPLIED
+  tag_2 CDATA #IMPLIED
+  tag_3 CDATA #IMPLIED
+  other CDATA #IMPLIED
+  form CDATA #IMPLIED
+  ref CDATA #IMPLIED">
+]>
+<!-- Time-stamp: <Wed Apr 30 03:43:02 2008 TrXMLBackend> -->
+<trees>
+<info>
+  <meta name="schema" content="Fslib::Schema=HASH(0xb4038a0)"/>
+</info>
+<types full="1">
+  <t n="token"/>
+  <t n="label"/>
+  <t n="tag_1"/>
+  <t n="tag_2"/>
+  <t n="tag_3"/>
+  <t n="other"/>
+  <t n="form"/>
+  <t n="ref"/>
+  <t n="ord"/>
+  <t n="dep"/>
+  <t n="afun"/>
+  <t n="tag"/>
+</types>
+"""
 
 if __name__ == '__main__':
 	file = codecs.open(sys.argv[1], encoding='utf-8')
-	Test(file, 'utf-8')
+	Dep2FS(file, 'utf-8')
 	file.close()
