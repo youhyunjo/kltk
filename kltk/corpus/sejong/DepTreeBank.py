5151fa28f580df6b0b4acdd1c6ae4d6046a8d98b
youhyunjo <youhyunjo@gmail.com>
2008-05-06 01:00:50 +0000

    edit kltk.corpus.sejong.DepTreeBank  - add some Test lines  - print paths
    git-svn-id: https://kltk.googlecode.com/svn/trunk@27 dc14f8f6-e949-0410-ab7c-05e6b6b323a2

--- kltk/corpus/sejong/DepTreeBank.py
+++ kltk/corpus/sejong/DepTreeBank.py
@@ -1,9 +1,20 @@
+# -*- coding: utf-8; tab-width: 4 -*-
 #!/usr/bin/python
+# Dependency Treebank 
+# $Id$ 
+""" Sejong Dependency Treebank, converted 
+from Sejong Parsed Corpus (distributed at 2007-11-12)
+by phr2dep of kltk.corpus.sejong.Parsed
+
+
+"""
+
 
 import codecs
 import sys
+import re
 
-class DepWalker:
+class ForestWalker:
 	def __init__(self, file):
 		self.file = file
 	
@@ -12,16 +23,57 @@ class DepWalker:
 	
 	def readtree(self):
 
+		# read sentence form line
 		line = self.readline()
 		tree = Tree(line.split(' ')[0])
 
+		# make list of nodes
+		list_of_nodes = []
 		line = self.readline()
 		while (line):
-			(ord, dep, tag1, tag2, word, morphs) = line.split("\t")	
-			tree.nodes.append(Node(ord, dep, tag1, tag2, word.strip(), morphs.strip()))
+			(ord, dep, tag1, tag2, wordform, morph_string) = line.split("\t")	
+			morphs = self.parse_morph_string(morph_string)
+			word = Word(ord, wordform, morphs)
+			list_of_nodes.append(Node(ord, dep, tag1, tag2, word))
 			line = self.readline()
 
+		# make tree with list of nodes
+		# set parent-child relations
+		for n in list_of_nodes:
+			if n.dep == n.ord:
+				tree.set_root(n)
+			else:
+				p = list_of_nodes[int(n.dep)-1]
+				n.parent = p
+				p.add_a_child(n)
+
+		tree.nodes = list_of_nodes
 		return tree
+	
+	def parse_morph_string(self, morph_string):
+		morphs = []
+
+		m = morph_string
+		for m in morph_string.split('+'):
+			m = m.strip()
+			if m == "" :
+				pass
+			else :
+				if m == "/SW":
+					form, pos = "+", "SW"
+				elif m[0:2] == "//":
+					form, pos = "/", m[2:]
+				else :
+					try :
+						form, pos = m.split("/")
+						if pos == "" : pos = "_ERR_"
+					except :
+						form, pos = m, "_ERR_"	
+				morphs.append(Morph(form,pos))
+		
+		return morphs
+
+
 
 	def readline(self):
 		line = self.file.readline()
@@ -41,20 +93,78 @@ class DepWalker:
 		return self.readtree()
 
 
+
+class TreeBank:
+	pass
+	
 class Tree:
 	def __init__(self, id):
 		self.id = id
 		self.nodes = []
+		self.root = None
+		self.terminals = []
+	
+	def set_root(self, node):
+		self.root = node
+	
+	def get_terminals(self):
+		if self.terminals :
+			pass
+		else:
+			for n in self.nodes:
+				if not n.children :
+					self.terminals.append(n)
+		return self.terminals
+
+
 
 class Node:
-	def __init__(self, ord, dep, tag1, tag2, word, morphs):
+	def __init__(self, ord, dep, tag1, tag2, word):
+		self.parent = None
+		self.children = []
 		self.ord = ord
 		self.dep = dep
 		self.tag1 = tag1
 		self.tag2 = tag2
+		self.form = word.form
 		self.word = word
-		self.morphs = morphs
+	
+	def add_a_child(self, node):
+		self.children.append(node)
 
+class Word:
+	def __init__(self, ord, form, morphs):
+		self.ord = ord
+		self.form = form
+		self.morphs = morphs
+	
+	def add_morph(self, morph):
+		self.morphs.append(morph)
+	
+	def has_pos(self, pos):
+		for m in self.morphs:
+			if m.pos == pos :
+				return True
+		return False
+
+	def __str__(self):
+		str = ""
+		for m in self.morphs:
+			if str == "":
+				str = m.form
+			elif m.pos[0] == "S" :
+				str += m.form
+			else :
+				str += "-" + m.form
+		return str
+		#return reduce(lambda x,y: x.form+"-"+y.form, self.morphs)
+
+
+class Morph:
+	def __init__(self, form, pos):
+		self.form = form
+		self.pos = pos
+ 
 
 #================
 class Encode:
@@ -68,13 +178,13 @@ class Encode:
 
 class Dep2FS:
 	def __init__(self, file, output_encoding):
-		self.dw = DepWalker(file)
+		self.fw = ForestWalker(file)
 		sys.stdout = Encode(sys.stdout, output_encoding)
 		self.dep2fs()
 		
 	def dep2fs(self) :
 		self.print_header()
-		for tree in self.dw:
+		for tree in self.fw:
 			self.print_nd_open(tree.id, "", "", "", 0, 0)
 			sys.stdout.write("(")
 			root = self.find_root(tree.nodes) 
@@ -126,13 +236,13 @@ class Dep2FS:
 
 class Dep2TrXML:
 	def __init__(self, file, output_encoding):
-		self.dw = DepWalker(file)
+		self.fw = ForestWalker(file)
 		sys.stdout = Encode(sys.stdout, output_encoding)
 		self.dep2trxml()
 		
 	def dep2trxml(self) :
 		self.print_header()
-		for tree in self.dw:
+		for tree in self.fw:
 			self.print_nd_open(tree.id, "", "", 0, 0)
 			root = self.find_root(tree.nodes) 
 			list = tree.nodes
@@ -201,7 +311,74 @@ class Dep2TrXML:
 </types>
 """
 
+class Test:
+	def __init__(self, file, output_encoding):
+		self.fw = ForestWalker(file)
+		sys.stdout = Encode(sys.stdout, output_encoding)
+		#self.print_emi_pair()
+		self.print_morph()
+	
+
+	def print_morph(self):
+		for tree in self.fw:
+			for n in tree.nodes:
+				for m in n.word.morphs:
+					print m.form + "/" + m.pos
+
+	def print_emi_pair(self):
+		for tree in self.fw:
+			for n in tree.nodes:
+				if n.word.has_pos("EC") \
+				   and n.parent is not None \
+				   and n.parent.word.has_pos("EC"):
+					sys.stdout.write(tree.id + " ; ")
+					for m in n.word.morphs:
+						if m.pos == "EC" :
+							sys.stdout.write(" -" + m.form)
+					sys.stdout.write(" < ")
+					for m in n.parent.word.morphs:
+						if m.pos == "EC" :
+							sys.stdout.write(" -" + m.form)
+					sys.stdout.write("\n")
+
+	def print_path_with_more_than_two_ECs(self):
+		for tree in self.fw:
+			#print tree.id
+			for n in tree.get_terminals() :
+				path = []
+				path.append(n)
+				while(n.parent) :
+					n = n.parent
+					path.append(n)
+				
+				# print path with more than two ECs
+				count = 0
+				for n in path:
+					if n.word.has_pos("EC") : count += 1 
+
+				if count > 1: 
+					sys.stdout.write(tree.id)
+					flag = False
+					for n in path:
+						if n.word.has_pos("EC") : flag = True
+						if flag : 
+							sys.stdout.write(" < ")
+							for m in n.word.morphs :
+								if m.pos == "EC":
+									sys.stdout.write("-" + m.form)
+					print ""
+
+
+
+			#self.print_node(tree.root, 0)
+
+	def print_node(self, node, depth):
+		print "\t"*depth + node.word
+		for n in node.children:
+			self.print_node(n, depth+1)
+	
+
 if __name__ == '__main__':
 	file = codecs.open(sys.argv[1], encoding='utf-8')
-	Dep2FS(file, 'utf-8')
+	Test(file, 'utf-8')
 	file.close()
